02c996b4dd81e84d368ab659489b430d4d4b5a92
Christopher <christopher@procedural.build>
2019-05-27 14:05:31 +0200

    jenkins and tests

--- test/es6.js
+++ test/es6.js
@@ -1,2 +1,39 @@
-import Traec from 'traec'
+import *  as chai from 'chai';
+import * as utils from '../src/utils/index'
+import * as permissions from '../src/utils/permissions/company'
+
+let assert = chai.assert;
+let expect = chai.expect;
+
+
+//Check the directories exist according to the paths they are routed with in the library
+it('Directories should exist and be routed correctly', function(){
+    expect('./src/redux/reducers').to.exist;
+    expect('./src/fetchBindings').to.exist;
+    expect('./src/fetchHandlers').to.exist;
+    expect('./src/immutable/utils').to.exist;
+    expect('./src/redux/reducers').to.exist;
+    expect('./src/utils/permissions').to.exist;
+
+
+
+});
+
+
+//Check functions exist within utils folder
+it('Functions in utils should exist', function(){
+    expect(utils.camelCaseToSentence).to.exist;   
+    expect(utils.getPath).to.exist;
+    expect(utils.keyListToSet).to.exist;
+    expect(utils.objToList).to.exist;
+    expect(utils.setItemInDictAndVis).to.exist;
+    expect(utils.setItemInListAndVis).to.exist;
+    expect(utils.setListInIndexedObj).to.exist;
+    expect(utils.setPath).to.exist;
+    expect(utils.setRelatedItems).to.exist;
+    expect(utils.sortObjListByKey).to.exist;
+    expect(utils.copyAlongPath).to.exist;
+
+});
+
 
