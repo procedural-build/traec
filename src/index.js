73f49fb01cf08f3257faa0cde74047f0ebf18bd5
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-05-28 17:57:48 +0200

    formatting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import * as fetchHandlers from './fetchHandlers';
-import * as fetchBindings from './fetchBindings';
-import {handlerMap} from './handlerMap';
-import {fetchRequired} from './fetchBindings/utils';
-import Fetch from './fetchManager'
-import Im from './immutable'
-import Redux from './redux'
+import * as fetchHandlers from "./fetchHandlers";
+import * as fetchBindings from "./fetchBindings";
+import { handlerMap } from "./handlerMap";
+import { fetchRequired } from "./fetchBindings/utils";
+import Fetch from "./fetchManager";
+import Im from "./immutable";
+import Redux from "./redux";
 
-export * from './handlerMap'
-export * from './fetchBindings/utils'
+export * from "./handlerMap";
+export * from "./fetchBindings/utils";
 
 export default {
-    fetchHandlers,
-    fetchBindings,
-    handlerMap,
-    fetchRequired,
-    Fetch,
-    Im,
-    Redux
-}
+  fetchHandlers,
+  fetchBindings,
+  handlerMap,
+  fetchRequired,
+  Fetch,
+  Im,
+  Redux
+};
 
 exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-  }
+  console.log("This is a message from the demo package");
+};
