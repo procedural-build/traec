724d87a82280cf0304f58bc4fedf466ddaed0702
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-05-29 16:34:41 +0200

    added test for failureHandler and throttling

--- test/apiMiddleware.test.js
+++ test/apiMiddleware.test.js
@@ -1,6 +1,9 @@
 import * as middleware from "../src/redux/apiMiddleware";
 import { fetchToState } from "../src/redux/actionCreators";
+import { hasFetched } from "../src/redux/fetchCache";
+import { fetchJSON } from "../src/redux/fetch";
 jest.mock("../src/redux/actionCreators");
+jest.mock("../src/redux/fetchCache");
 
 describe("Response Types", () => {
   it("should handle default type", () => {
@@ -28,29 +31,54 @@ describe("Response Types", () => {
   });
 });
 
+describe("Record Fetch", () => {
+  it("should call dispatch once", () => {
+    const fetchParams = {};
+    const dispatch = jest.fn();
+    const firstCall = {
+      type: "FETCH_SET_SENT",
+      fetchParams
+    };
+    middleware.recordFetch(fetchParams, dispatch);
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith(firstCall);
+  });
+});
+
 describe("Failure Handler", () => {
-  it("should handle default type", () => {
-    const APICall = {
-      defaultType: "ENTITY_SET_FUNC"
-    };
-    const expectedBody = {
-      failureType: "ENTITY_SET_FUNC",
-      successType: "ENTITY_SET_FUNC"
-    };
-    expect(middleware.responseTypes(APICall)).toEqual(expectedBody);
+  it("should call dispatch twice", () => {
+    const failureType = "";
+    const fetchParams = {};
+    const stateParams = {};
+    const dispatch = jest.fn();
+    const error = "This is an error";
+
+    middleware.failureHandler(error, failureType, fetchParams, stateParams, dispatch);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
   });
 
-  it("should handle success & failure", () => {
-    const APICall = { failureType: "LOGIN_FAILURE", successType: "LOGIN_SUCCESS" };
-    const expectedBody = { failureType: "LOGIN_FAILURE", successType: "LOGIN_SUCCESS" };
+  it("should call dispatch with FETCH_FAIL and failureType", () => {
+    const failureType = "FAIL";
+    const fetchParams = {};
+    const stateParams = {};
+    const dispatch = jest.fn();
+    const error = "This is an error";
+    const firstCall = {
+      type: "FETCH_FAIL",
+      fetchParams
+    };
+    const secondCall = {
+      type: failureType,
+      payload: { errors: error },
+      stateParams,
+      fetchParams
+    };
+    middleware.failureHandler(error, failureType, fetchParams, stateParams, dispatch);
 
-    expect(middleware.responseTypes(APICall)).toEqual(expectedBody);
-  });
-
-  it("should throw error if no type", () => {
-    const APICall = { defaultType: null };
-
-    expect(() => middleware.responseTypes(APICall)).toThrowError();
+    expect(dispatch).toHaveBeenCalledWith(firstCall);
+    expect(dispatch).toHaveBeenCalledWith(secondCall);
   });
 });
 
@@ -125,27 +153,22 @@ describe("SuccessHandler", () => {
 });
 
 describe("check Throttling", () => {
-  it("should handle default type", () => {
-    const APICall = {
-      defaultType: "ENTITY_SET_FUNC"
-    };
-    const expectedBody = {
-      failureType: "ENTITY_SET_FUNC",
-      successType: "ENTITY_SET_FUNC"
-    };
-    expect(middleware.responseTypes(APICall)).toEqual(expectedBody);
+  const action = "This is an action";
+  const getState = jest.fn();
+  const fetchParams = {};
+
+  it("if fetched, it should call next", () => {});
+
+  it("if not fetched, it should not do anything", () => {
+    spyOn(console, "log");
+    hasFetched.mockImplementation((getstate, fetchParams, time) => false);
+    middleware.checkThrottling(getState, fetchParams, action);
+
+    expect(hasFetched).toHaveBeenCalled();
+    expect(console.log).not.toHaveBeenCalled();
   });
+});
 
-  it("should handle success & failure", () => {
-    const APICall = { failureType: "LOGIN_FAILURE", successType: "LOGIN_SUCCESS" };
-    const expectedBody = { failureType: "LOGIN_FAILURE", successType: "LOGIN_SUCCESS" };
-
-    expect(middleware.responseTypes(APICall)).toEqual(expectedBody);
-  });
-
-  it("should throw error if no type", () => {
-    const APICall = { defaultType: null };
-
-    expect(() => middleware.responseTypes(APICall)).toThrowError();
-  });
+describe("apiMiddleware", () => {
+  //Should be tested at a higher level
 });
