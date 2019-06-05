7693d64cc9ff47c09f29227198f9fada1c93cf5d
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-06-05 13:55:46 +0200

    formatting

--- src/fetchHandlers/treeDescription.js
+++ src/fetchHandlers/treeDescription.js
@@ -1,21 +1,18 @@
-export const postDescription= ({trackerId, refId, commitId, treeId}) => {
-    const fetchParams = {
-        method: 'POST',
-        url: `/api/tracker/${trackerId}/ref/${refId}/tree/${treeId}/description/`,
+export const postDescription = ({ trackerId, refId, commitId, treeId }) => {
+  const fetchParams = {
+    method: "POST",
+    url: `/api/tracker/${trackerId}/ref/${refId}/tree/${treeId}/description/`
+  };
+  const stateSetFunc = (state, action) => {
+    const data = action.payload;
+    let { formVisPath, formObjPath } = action.stateParams;
+    let newState = state.setInPath(formObjPath, data);
+    if (!data.errors) {
+      newState = newState.addToDict("descriptions.byId", data);
+      newState = newState.setInPath(formVisPath, false);
+      newState = newState.addListToSets([`commitEdges.byId.${commitId}.trees.${treeId}.descriptions`], [data.uid]);
     }
-    const stateSetFunc= (state, action) => {
-        const data = action.payload
-        let { formVisPath, formObjPath } = action.stateParams
-        let newState = state.setInPath( formObjPath, data)
-        if (!data.errors) { 
-            newState = newState.addToDict( 'descriptions.byId', data)
-            newState = newState.setInPath( formVisPath, false)
-            newState = newState.addListToSets( 
-                [`commitEdges.byId.${commitId}.trees.${treeId}.descriptions`], 
-                [data.uid]
-            )
-        }
-        return newState
-    }
-    return {fetchParams, stateParams: {stateSetFunc}}
-}
+    return newState;
+  };
+  return { fetchParams, stateParams: { stateSetFunc } };
+};
