e7fc07294c5973b8ccc00476ffc43988fcdd735d
Mark Pitman <mark@ods-engineering.com>
2020-02-05 15:45:07 +0100

    Removed refresh token from localStorage

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -10,29 +10,34 @@ const initialState = Im.fromJS({
 });
 
 class TokenRefresher {
+  /*NOTE: that the refresh token is NOT stored in localStorage for security
+    This is set in a httpOnly cookie by the server at the `/jwt-auth/get/` endpoint
+    and is issued only to the `/jwt-auth/refresh/` endpoint on the same domain.
+
+    So we burn the refresh token here - it is not stored anywhere accessible to JavaScript.
+    
+    We trust that the browser will securely store the httpOnly cookie with the appropriate
+    refresh token.
+  */
+
   constructor() {
     this.intervals = {};
-
     this.doRefresh = this.doRefresh.bind(this);
   }
 
-  doRefresh(refreshToken) {
+  doRefresh() {
     // Dispatch an action that will be caught by the APIMiddleware for fetching
     console.log("Requesting refresh for JWT token");
-    // Get the token-refresh
-    let fetchParams = {
-      url: "/auth-jwt/refresh/",
-      method: "POST",
-      body: {
-        refresh: refreshToken
-      }
-    };
     store.dispatch({
       APICallTypes: {
         successType: "LOGIN_SUCCESS",
         failureType: "LOGIN_FAILURE"
       },
-      fetchParams
+      fetchParams: {
+        url: "/auth-jwt/refresh/",
+        method: "POST",
+        body: { refresh: null } // The refresh token should be in the httpOnly cookie that is sent
+      }
     });
   }
 
@@ -43,14 +48,14 @@ class TokenRefresher {
     console.log("Refreshing access token again in ", msRemaining / 1000, "seconds");
     let id = setInterval(
       refreshToken => {
-        // Only do it once
+        // Only do it once - the next access token will set another interval
         this.doRefresh(refreshToken);
         clearInterval(id);
       },
       msRemaining,
       refreshToken
     );
-    this.intervals[id] = refreshToken;
+    this.intervals[id] = true;
   }
 
   clearAll() {
@@ -67,37 +72,39 @@ export default function(state = initialState, action) {
   //console.log("Reducing auth data")
   switch (action.type) {
     case types.LOGIN_SUCCESS:
-      // Try to get the token from either "token" or "access" keys
-      let token = action.payload.access || action.payload.token;
-      let refresh = action.payload.refresh || localStorage.getItem("token-refresh");
+      // Try to get the access token from either "token" or "access" keys
+      let { access, token } = action.payload;
+      token = access || token;
       // If we have a new token then store that in localStorage
       if (token) {
         localStorage.setItem("token", token);
-        // Store the refreshToken in localStorage also
       } else {
         token = localStorage.getItem("token");
-      }
-      // Store the refresh token in localStorage also (if provided)
-      if (action.payload.refresh) {
-        localStorage.setItem("token-refresh", refresh);
       }
       // Decode the access token for display in Redux
       let decoded_token = token ? jwt_decode(token) : null;
       // Set a timer to request a new token
-      if (decoded_token.exp && refresh) {
+      if (decoded_token.exp) {
         TOKEN_REFRESHER.clearAll();
-        TOKEN_REFRESHER.setRefresh(decoded_token.exp, refresh);
+        TOKEN_REFRESHER.setRefresh(decoded_token.exp);
       }
       // Add this data to your Redux
+      for (let key of ["access", "refresh", "token"]) {
+        delete action.payload[key];
+      }
+      // Get a mock user object
+      let user = { ...decoded_token };
+      for (let key of ["token_type", "exp", "jti"]) {
+        delete user[key];
+      }
       return state.merge(
         Im.fromJS({
           ...action.payload,
           token,
-          access: token,
-          refresh: refresh,
           isAuthenticated: true,
           status: "confirmed",
-          decoded_token
+          decoded_token,
+          user
         })
       );
 
