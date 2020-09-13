0ff4feb7c0b521931b9613d14a2dceb418491e82
Christian Kongsgaard <christian@procedural.build>
2020-09-13 17:50:06 +0200

    updated HTMLText

--- src/utils/html.js
+++ src/utils/html.js
@@ -3,5 +3,11 @@ import DOMPurify from "dompurify";
 
 export const HTMLText = props => {
   const cleanText = DOMPurify.sanitize(props.text);
-  return <div style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{ __html: cleanText }} />;
+  return (
+    <div
+      className={props.extraClassName}
+      style={{ whiteSpace: "pre-wrap" }}
+      dangerouslySetInnerHTML={{ __html: cleanText }}
+    />
+  );
 };
