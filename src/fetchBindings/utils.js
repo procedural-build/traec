44a5b78b15a2cb9a21559eae708291ad2813e6cd
Christian Kongsgaard <christian@procedural.build>
2020-10-02 15:44:28 +0200

    added filter to filter out empty fetches

--- src/fetchBindings/utils.js
+++ src/fetchBindings/utils.js
@@ -1,7 +1,9 @@
 export const fetchRequired = function(fetches = this.requiredFetches) {
-  fetches.map(fetch =>
-    fetch.dispatchFromProps(this.props, this.state ? this.state.fetchedUrls : {}, i => {
-      return this.setState ? this.setState(i) : null;
-    })
-  );
+  fetches
+    .filter(fetch => fetch)
+    .map(fetch =>
+      fetch.dispatchFromProps(this.props, this.state ? this.state.fetchedUrls : {}, i => {
+        return this.setState ? this.setState(i) : null;
+      })
+    );
 };
