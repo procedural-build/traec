95500829520a2b1984901ea0eac37b80f97ac0af
Christian Kongsgaard <christian@procedural.build>
2021-04-07 21:01:49 +0200

    updated dispatch handler

--- src/fetchHandlers/tenant/dispatch.js
+++ src/fetchHandlers/tenant/dispatch.js
@@ -4,14 +4,17 @@ export const postTenantDispatch = ({}) => {
     url: `/api/tenant/admin/dispatch/`,
     apiId: "api_tenant_admin_dispatch_create",
     requiredParams: [],
-    queryParams: {}
+    queryParams: {},
   };
   const stateSetFunc = (state, action) => {
     const data = action.payload;
+    if (data.errors) {
+      return state.setInPath(`dispatch.errors.${action.fetchParams.body.type}`, data.errors);
+    }
     return state;
   };
   return {
     fetchParams,
-    stateParams: { stateSetFunc }
+    stateParams: { stateSetFunc },
   };
 };
