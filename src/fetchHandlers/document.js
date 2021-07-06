1be5eaf595d03c12cfe525b7b3138b2940e2a2c9
Mark Pitman <mark@procedural.build>
2021-07-06 11:19:18 +0200

    Updated tracker_documents endpoint fetchHandler

--- src/fetchHandlers/document.js
+++ src/fetchHandlers/document.js
@@ -1,3 +1,4 @@
+import { INSPECT_MAX_BYTES } from "buffer";
 import Crypto from "crypto";
 
 export const postDocument = ({ trackerId, refId, commitId, treeId }) => {
@@ -5,7 +6,7 @@ export const postDocument = ({ trackerId, refId, commitId, treeId }) => {
     method: "POST",
     url: `/api/tracker/${trackerId}/ref/${refId}/tree/${treeId}/document/`,
     apiId: "api_tracker_ref_tree_document_create",
-    requiredParams: ["trackerId", "refId", "commitId", "treeId"],
+    requiredParams: ["trackerId", "refId", "commitId", "treeId"]
   };
   const stateSetFunc = (state, action) => {
     const data = action.payload;
@@ -34,23 +35,25 @@ export const postDocument = ({ trackerId, refId, commitId, treeId }) => {
   };
   //Modify the POST before send to structure it as required for the API
   Object.assign(fetchParams, {
-    preFetchHook: (body) => {
+    preFetchHook: body => {
       return {
-        name: Crypto.createHash("sha1").update(body.title).digest("hex"),
+        name: Crypto.createHash("sha1")
+          .update(body.title)
+          .digest("hex"),
         description: {
           title: body.title,
-          text: body.description,
-        },
+          text: body.description
+        }
       };
-    },
+    }
   });
   return {
     fetchParams,
     stateParams: {
       stateSetFunc,
       formVisPath: `documents.editById.${treeId}.SHOW_DOC_FORM`,
-      formObjPath: `documents.editById.${treeId}.newItem`,
-    },
+      formObjPath: `documents.editById.${treeId}.newItem`
+    }
   };
 };
 
@@ -61,7 +64,7 @@ export const getDocumentObjects = ({ trackerId, commitId, docId, thisCommitOnly 
     url: `/api/tracker/${trackerId}/commit/${commitId}/document/${docId}/object/${queryParams}`,
     apiId: "api_tracker_commit_document_object_list",
     requiredParams: ["trackerId", "commitId", "docId"],
-    queryParams: { thisCommitOnly: null },
+    queryParams: { thisCommitOnly: null }
   };
   const stateSetFunc = (state, action) => {
     // Successful put returns a DocumentStatusSerializer object
@@ -87,7 +90,7 @@ export const getDocumentObject = ({ trackerId, commitId, docId, docObjectId, sig
     url: `/api/tracker/${trackerId}/commit/${commitId}/document/${docId}/object/${docObjectId}/${queryParams}`,
     apiId: "api_tracker_commit_document_object_retrieve",
     requiredParams: ["trackerId", "commitId", "docId", "docObjectId"],
-    queryParams: { signedURL: false },
+    queryParams: { signedURL: false }
   };
   const stateSetFunc = (state, action) => {
     // Successful put returns a DocumentStatusSerializer object
@@ -105,7 +108,7 @@ export const deleteDocumentObject = ({ trackerId, commitId, docId, docObjectId }
     url: `/api/tracker/${trackerId}/commit/${commitId}/document/${docId}/object/${docObjectId}/`,
     apiId: "api_tracker_commit_document_object_delete",
     requiredParams: ["trackerId", "commitId", "docId", "docObjectId"],
-    queryParams: {},
+    queryParams: {}
   };
   const stateSetFunc = (state, action) => {
     return state.deleteIn([`docObjects`, `byId`, `${docObjectId}`]);
@@ -130,13 +133,13 @@ export const putDocumentObject = ({ trackerId, refId, commitId, documentId, allo
     method: "PUT",
     url: `/api/tracker/${trackerId}/ref/${refId}/document/${documentId}/${getDocQueryParams({
       allow_commit_change,
-      treeId,
+      treeId
     })}`,
     apiId: "api_tracker_ref_document_update",
     requiredParams: ["trackerId", "refId", "commitId", "documentId"],
     queryParams: { allow_commit_change: false, treeId: null },
     headers: { "content-type": null },
-    rawBody: true,
+    rawBody: true
   };
   const stateSetFunc = (state, action) => {
     // Successful put returns a DocumentStatusSerializer object
@@ -158,7 +161,7 @@ export const putDocumentObject = ({ trackerId, refId, commitId, documentId, allo
       if (description) {
         newState = newState.addToDict("descriptions.byId", description);
         newState = newState.addListToSet(`commitEdges.byId.${commitId}.documents.${documentId}.descriptions`, [
-          description.uid,
+          description.uid
         ]);
       }
       // Finally hide the form
@@ -171,8 +174,8 @@ export const putDocumentObject = ({ trackerId, refId, commitId, documentId, allo
     stateParams: {
       stateSetFunc,
       formVisPath: `documents.editById.${documentId}.SHOW_EDIT_DESCRIPTION_FORM`,
-      formObjPath: `documents.editById.${documentId}.editDescription`,
-    },
+      formObjPath: `documents.editById.${documentId}.editDescription`
+    }
   };
 };
 
@@ -183,15 +186,15 @@ export const putDocumentObjectCommit = ({ trackerId, commitId, documentId, allow
     commitId,
     documentId,
     allow_commit_change,
-    treeId,
+    treeId
   });
   Object.assign(fetchParams, {
     url: `/api/tracker/${trackerId}/commit/${commitId}/document/${documentId}/${getDocQueryParams({
       allow_commit_change,
-      treeId,
+      treeId
     })}`,
     apiId: "api_tracker_commit_document_update",
-    requiredParams: ["trackerId", "commitId", "documentId"],
+    requiredParams: ["trackerId", "commitId", "documentId"]
   });
   return { fetchParams, stateParams };
 };
@@ -201,12 +204,12 @@ export const deleteDocument = ({ trackerId, refId, commitId, docId, treeId }) =>
     method: "DELETE",
     url: `/api/tracker/${trackerId}/ref/${refId}/document/${docId}/`,
     apiId: "api_tracker_ref_document_delete",
-    requiredParams: ["trackerId", "refId", "commitId", "docId"],
+    requiredParams: ["trackerId", "refId", "commitId", "docId"]
   };
 
   return {
     fetchParams,
-    stateParams: { stateSetFunc: (state) => deleteDocumentFromState(state, treeId, commitId, docId) },
+    stateParams: { stateSetFunc: state => deleteDocumentFromState(state, treeId, commitId, docId) }
   };
 };
 
@@ -215,12 +218,12 @@ export const deleteTreeDocument = ({ trackerId, treeId, commitId, docId }) => {
     method: "DELETE",
     url: `/api/tracker/${trackerId}/commit/${commitId}/tree/${treeId}/document/${docId}/`,
     apiId: "api_tracker_commit_tree_document_delete",
-    requiredParams: ["trackerId", "treeId", "commitId", "docId"],
+    requiredParams: ["trackerId", "treeId", "commitId", "docId"]
   };
 
   return {
     fetchParams,
-    stateParams: { stateSetFunc: (state) => deleteDocumentFromState(state, treeId, commitId, docId) },
+    stateParams: { stateSetFunc: state => deleteDocumentFromState(state, treeId, commitId, docId) }
   };
 };
 
@@ -242,47 +245,53 @@ const deleteDocumentFromState = (state, treeId, commitId, docId) => {
 
   newState = newState.removeInPath(`documents.byId.${docId}`);
   if (parentId) {
-    newState = newState.updateIn(`commitEdges.byId.${commitId}.trees.${parentId}.trees`.split("."), (i) =>
+    newState = newState.updateIn(`commitEdges.byId.${commitId}.trees.${parentId}.trees`.split("."), i =>
       i ? i.delete(docId) : null
     );
   }
   return newState;
 };
 
-export const getDisciplineDocuments = ({ trackerId, all_disciplines = false }) => {
+export const getDisciplineDocuments = ({ trackerId, userId = null, all_disciplines = false }) => {
+  let query_params = all_disciplines ? "?all_disciplines=true" : "";
   const fetchParams = {
     method: "GET",
-    url: `/api/tracker/${trackerId}/documents/${all_disciplines ? "?all_disciplines=true" : ""}`,
+    url: `/api/tracker/${trackerId}/documents/${query_params}`,
     apiId: "api_tracker_documents_list",
     requiredParams: ["trackerId"],
-    queryParams: { all_disciplines: false },
+    queryParams: { all_disciplines: false }
   };
   const stateSetFunc = (state, action) => {
     // Successful put returns a DocumentStatusSerializer object
     let data = action.payload;
     let newState = state;
-    let trackerId = action.fetchParams.url.split("/")[3];
     for (let item of data) {
-      if (item) {
-        let statusId = item.status ? item.status.uid : null;
-        let descriptionId = item.description ? item.description.uid : null;
-        let refId = item.cref ? item.cref.uid : null;
-        let document = { uid: item.uid, status: statusId, description: descriptionId, trackerId, refId: refId };
+      if (!item) {
+        continue;
+      }
 
-        // Store the nested current_object separately and refer to only uuid in status
-        newState = newState.addToDict("user.documents.byId", document);
-        if (descriptionId) newState = newState.addToDict("descriptions.byId", item.description);
+      // Store the nested current_object separately and refer to only uuid in status
+      newState = newState.addToDict(`user.documents.${trackerId}.${userId || "master"}`, {
+        ...item,
+        status: item.status?.uid,
+        description: item.description?.uid,
+        _commit: item.commit_id,
+        _path: item.path
+      });
 
-        if (statusId) {
-          let current_object_id = item.status.current_object ? item.status.current_object.uid : null;
-          let status = { ...item.status, current_object: current_object_id };
-          newState = newState.addToDict("docStatuses.byId", status);
-          if (current_object_id) {
-            newState = newState.addToDict("docObjects.byId", item.status.current_object);
-          }
-        }
-        if (refId) {
-          newState = newState.addToDict("refs.byId", item.cref);
+      // Flatten and store the description
+      if (item.description) {
+        newState = newState.addToDict("descriptions.byId", item.description);
+      }
+
+      // Flatten and store the nested status
+      if (item.status) {
+        newState = newState.addToDict("docStatuses.byId", {
+          ...item.status,
+          current_object: item.status.current_object?.uid
+        });
+        if (item.status.current_object) {
+          newState = newState.addToDict("docObjects.byId", item.status.current_object);
         }
       }
     }
