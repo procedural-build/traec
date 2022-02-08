903990906e4e1ee86a7aeceb1bb71da0e683f744
Mark Pitman <mark@procedural.build>
2022-02-08 15:32:34 +0100

    Updated project_inputs fetch handler

--- src/fetchHandlers/project/projectInputs.js
+++ src/fetchHandlers/project/projectInputs.js
@@ -1,16 +1,21 @@
-export const getProjectInputs = ({ projectId, format = "excel" }) => {
+export const getProjectInputs = ({ projectId, refId = null, output_format = "excel" }) => {
+  let query_string = new URLSearchParams({
+    ...(refId ? { refId } : {}),
+    output_format
+  }).toString();
+
   const fetchParams = {
     method: "GET",
-    url: `/api/project/${projectId}/inputs/`,
+    url: `/api/project/${projectId}/inputs/?${query_string}`,
     apiId: "api_project_inputs_list",
     requiredParams: ["projectId"],
-    queryParms: {}
+    queryParms: { refId: null, output_format: "excel" }
   };
   let stateSetFunc = (state, action) => {
     return state;
   };
   // Adjust the headers based on if the format is excel
-  if (format == "excel") {
+  if (output_format == "excel") {
     Object.assign(fetchParams, { headers: { "content-type": "application/xlsx" } });
     stateSetFunc = (state, action) => {
       return state;
