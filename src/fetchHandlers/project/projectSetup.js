08ad88985d428e0374b5bd2fe2b7ad3379847d31
Mark Pitman <mark@procedural.build>
2022-05-18 19:07:19 +1000

    Updated project_and_setup handler

--- src/fetchHandlers/project/projectSetup.js
+++ src/fetchHandlers/project/projectSetup.js
@@ -8,7 +8,7 @@ HANDLER FOR POSTING A PROJECT AND SETTING IT UP WITH A TRACKER AND
 SOME ROOT BRANCHES
 */
 
-export const postProjectAndSetup = () => {
+export const postProjectAndSetup = ({ postTrackerSetupSuccessHook }) => {
   /* Handler to create a project and then set it up by adding a tracker
     and a category for conversion factors
     */
@@ -26,11 +26,12 @@ export const postProjectAndSetup = () => {
         if (orgpost.from_template) {
           Object.assign(body, {
             from_template: orgpost.from_template,
-            include_template_subcategories: orgpost.include_subcategories || false,
+            include_template_subcategories: orgpost.include_subcategories || false
           });
         }
         Object.assign(fetchParams, {
           body,
+          postSuccessHook: postTrackerSetupSuccessHook || (() => {}),
           // After creating the tracker, then setup the conversion_factor branch
           nextHandlers: orgpost.from_template
             ? []
@@ -39,8 +40,8 @@ export const postProjectAndSetup = () => {
                   let { fetchParams, stateParams } = postRootRef({ trackerId: data.uid });
                   Object.assign(fetchParams, { body: { root_tree_name: "conversion_factors" } });
                   return { fetchParams, stateParams };
-                },
-              ],
+                }
+              ]
         });
         return { fetchParams, stateParams };
       },
@@ -56,12 +57,12 @@ export const postProjectAndSetup = () => {
               "READ_PROJECT_REPORT",
               "CREATE_TRACKER_REF_SCORE_VALUE",
               "READ_TRACKER_REF_SCORE_VALUE",
-              "UPDATE_TRACKER_REF_SCORE_VALUE",
-            ],
-          },
+              "UPDATE_TRACKER_REF_SCORE_VALUE"
+            ]
+          }
         };
         Object.assign(fetchParams, {
-          body,
+          body
         });
         return { fetchParams, stateParams };
       },
@@ -76,12 +77,12 @@ export const postProjectAndSetup = () => {
               "READ_PROJECT_REPORT",
               "CREATE_TRACKER_REF_SCORE_VALUE",
               "READ_TRACKER_REF_SCORE_VALUE",
-              "UPDATE_TRACKER_REF_SCORE_VALUE",
-            ],
-          },
+              "UPDATE_TRACKER_REF_SCORE_VALUE"
+            ]
+          }
         };
         Object.assign(fetchParams, {
-          body,
+          body
         });
         return { fetchParams, stateParams };
       },
@@ -90,15 +91,15 @@ export const postProjectAndSetup = () => {
         let body = {
           name: "Viewer",
           policy_json: {
-            actions: ["READ_TRACKER_REF", "READ_PROJECT_MEMBER", "READ_PROJECT_REPORT", "READ_TRACKER_REF_SCORE_VALUE"],
-          },
+            actions: ["READ_TRACKER_REF", "READ_PROJECT_MEMBER", "READ_PROJECT_REPORT", "READ_TRACKER_REF_SCORE_VALUE"]
+          }
         };
         Object.assign(fetchParams, {
-          body,
+          body
         });
         return { fetchParams, stateParams };
-      },
-    ],
+      }
+    ]
   });
   return { fetchParams, stateParams };
 };
